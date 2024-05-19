e8f395f265a84d99b84c80517d35e245dab97414
Christian L. Viana <viana2015christian@gmail.com>
2024-05-19 18:09:05 -0300

    Update cart-icon.jsx

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,9 +1,9 @@
-import { ReactComponent as ShoppingIcon}  from '../../assets/shopping-cart-simple.svg'
+import  ShoppingIcon  from '../../assets/shopping-cart-simple.svg'
 
 export function CartIcon() {
   return (
     <div className="cart-icon-container">
-      <ShoppingIcon className="shopping-icon" />
+      <img src={ShoppingIcon} className="shopping-icon" />
     </div>
   )
 }

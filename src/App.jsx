489694ab2bb9349e31f2aab9a3b99088bf177f78
Christian Viana <viana2015christian@gmail.com>
2024-05-26 19:09:48 -0300

    Aula de useState e useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.scss'
 import { Auth } from './router/auth/auth'
 import { Home } from './router/home/home'
 import { Navigation } from './router/navigation/navigation'
+import { Shop } from './router/shop/shop'
 export function App() {
   return (
     <>
       <Navigation />
-      <Home />
+      <Home /> 
       <Auth />
+      <Shop />
     </>
   )
 }

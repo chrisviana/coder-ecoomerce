9ba2630a779f451e611fd0d25c251cf9551f4a7c
Christian Viana <viana2015christian@gmail.com>
2024-05-29 19:35:37 -0300

    Aula de rotas

--- src/router/navigation/navigation.jsx
+++ src/router/navigation/navigation.jsx
@@ -1,19 +1,28 @@
+import { Outlet } from 'react-router-dom'
 import { CartIcon } from '../../components/cart-icon/cart-icon'
 import './navigation.scss'
+import { NavLink } from 'react-router-dom'
 
 export function Navigation() {
   return (
     <>
       <div className="navigation">
         <div className='logo-container'>
-          <h1>Coder E-commerce</h1>
+          <NavLink to="/">
+            <h1>Coder E-commerce</h1>
+          </NavLink>
         </div>
         <div className="nav-links-container">
-          <a className="nav-link">SHOP</a>
-          <a className="nav-link">LOGIN</a>
+          <NavLink className="nav-link" to="/shop">
+            SHOP
+          </NavLink>
+          <NavLink className="nav-link" to="/auth">
+            LOGIN
+          </NavLink>
           <CartIcon />
         </div>
       </div>
+      <Outlet />
     </>
   )
 }
